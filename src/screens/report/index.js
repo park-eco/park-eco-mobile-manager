5bb240479ad2674b6aac7719ea7b373a6b50e629
Lee Khai <leekhai18@gmail.com>
2018-11-28 00:34:31 +0700

    Init data for pickers

--- src/screens/report/index.js
+++ src/screens/report/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Dimensions, ScrollView, View, TextInput, TouchableOpacity } from "react-native";
+import { Dimensions, ScrollView, View } from "react-native";
 import {
 	Container,
 	Header,
@@ -28,26 +28,31 @@ const chartConfig = {
 	color: (opacity = 1) => `rgba(${255}, ${255}, ${255}, ${opacity})`
 }
 
+const parkings = ['Suối Tiên', 'Đầm Sen', 'CV Lê Thị Riêng', 'Coop Extra Linh Trung'];
+const types = ['Daily', 'Weekly', 'Monthly'];
+
+const date = new Date();
+const currentDate = date.getDate();
+const currentMonth = date.getMonth() + 1;
+const currentYear = date.getFullYear();
+
 class Report extends Component {
 	constructor(props, context) {
 		super(props, context);
 		this.state = {
-			selectedParking: this._createParkingData()[0],
-		}
-	}
-
-	_createParkingData() {
-		let data = [];
-		for (var i = 0; i < 100; i++) {
-			data.push(i);
+			selectedParking: parkings[0],
+			selectedType: types[0],
+			selectedDateFrom: `${currentYear}-${currentMonth}-${currentDate - 1}`,
+			selectedDateTo: `${currentYear}-${currentMonth}-${currentDate}`,
 		}
 
-		return data;
+		this._showDatePicker = this._showDatePicker.bind(this);
 	}
 
 	_showParkingPicker() {
 		Picker.init({
-			pickerData: this._createParkingData(),
+			pickerTitleText: 'Select The Parking',
+			pickerData: parkings,
 			selectedValue: [0],
 			onPickerConfirm: data => {
 				this.setState({ selectedParking: data });
@@ -60,7 +65,84 @@ class Report extends Component {
 
 		Picker.show();
 	}
-	
+
+	_showTypePicker() {
+		Picker.init({
+			pickerTitleText: 'Select Report Type',
+			pickerData: types,
+			selectedValue: [0],
+			onPickerConfirm: data => {
+				this.setState({ selectedType: data });
+			},
+			onPickerCancel: data => {
+			},
+			onPickerSelect: data => {
+			}
+		});
+
+		Picker.show();
+	}
+
+	_createDateData() {
+		let date = [];
+		for (let i = 2015; i < 2099; i++) {
+			let month = [];
+			for (let j = 1; j < 13; j++) {
+				let day = [];
+				if (j === 2) {
+					for (let k = 1; k < 29; k++) {
+						day.push(k);
+					}
+					//Leap day for years that are divisible by 4, such as 2000, 2004
+					if (i % 4 === 0) {
+						day.push(29);
+					}
+				}
+				else if (j in { 1: 1, 3: 1, 5: 1, 7: 1, 8: 1, 10: 1, 12: 1 }) {
+					for (let k = 1; k < 32; k++) {
+						day.push(k);
+					}
+				}
+				else {
+					for (let k = 1; k < 31; k++) {
+						day.push(k);
+					}
+				}
+
+				let _month = {};
+				_month[j] = day;
+
+				month.push(_month);
+			}
+
+			let _date = {};
+			_date[i] = month;
+
+			date.push(_date);
+		}
+
+		return date;
+	}
+
+	_showDatePicker(forWhat) {
+		Picker.init({
+			pickerTitleText: 'Select Date ' + forWhat,
+			pickerData: this._createDateData(),
+			onPickerConfirm: (pickedValue, pickedIndex) => {
+				if (forWhat === 'From') {
+					this.setState({ selectedDateFrom: `${pickedValue[0]}-${pickedValue[1]}-${pickedValue[2]}` });
+				} else if (forWhat === 'To') {
+					this.setState({ selectedDateTo: `${pickedValue[0]}-${pickedValue[1]}-${pickedValue[2]}` });
+				}
+			},
+			onPickerCancel: (pickedValue, pickedIndex) => {
+			},
+			onPickerSelect: (pickedValue, pickedIndex) => {
+			}
+		});
+		Picker.show();
+	}
+
 
 	render() {
 		const width = Dimensions.get('window').width
@@ -119,8 +201,8 @@ class Report extends Component {
 								backgroundColor: chartConfig.backgroundColor,
 								width: width * 0.3,
 							}}
-							onPress={this._showParkingPicker.bind(this)}>
-							<Text>{this.state.selectedParking}</Text>
+							onPress={this._showTypePicker.bind(this)}>
+							<Text>{this.state.selectedType}</Text>
 							<Icon name='arrow-down' />
 						</Button>
 					</View>
@@ -135,8 +217,8 @@ class Report extends Component {
 								backgroundColor: chartConfig.backgroundColor,
 								width: width * 0.48,
 							}}
-							onPress={this._showParkingPicker.bind(this)}>
-							<Text>{this.state.selectedParking}</Text>
+							onPress={() => this._showDatePicker('From')}>
+							<Text>{this.state.selectedDateFrom}</Text>
 							<Icon name='arrow-down' />
 						</Button>
 
@@ -145,8 +227,8 @@ class Report extends Component {
 								backgroundColor: chartConfig.backgroundColor,
 								width: width * 0.48,
 							}}
-							onPress={this._showParkingPicker.bind(this)}>
-							<Text>{this.state.selectedParking}</Text>
+							onPress={() => this._showDatePicker('To')}>
+							<Text>{this.state.selectedDateTo}</Text>
 							<Icon name='arrow-down' />
 						</Button>
 					</View>
