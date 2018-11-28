0ec30c397df3784ade6a8bc82821611a44ac2f48
Lee Khai <leekhai18@gmail.com>
2018-11-28 08:31:00 +0700

    Config release

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {AppRegistry} from 'react-native';
-// import App from './App';
-import App from './src/screens/report/index';
+import App from './App';
+// import App from './src/screens/report/index';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- ios/ParkEcoManager/AppDelegate.m
+++ ios/ParkEcoManager/AppDelegate.m
@@ -19,7 +19,11 @@
 
   NSURL *jsCodeLocation;
 
+#ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ParkEcoManager"
