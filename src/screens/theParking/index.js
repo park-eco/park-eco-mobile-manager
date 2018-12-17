8662914a559a4b69f07e3d714606095e5b223cdd
Lam Le <15520405@gm.uit.edu.vn>
2018-12-17 22:39:01 +0700

    Integrating geting parking lot data API call

--- src/screens/theParking/index.js
+++ src/screens/theParking/index.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
 import {
-  Container,
-  Header,
-  Title,
-  Content,
-  Text,
-  Button,
-  Icon,
-  Left,
-  Right,
-  Body
+	Container,
+	Header,
+	Title,
+	Content,
+	Text,
+	Button,
+	Icon,
+	Left,
+	Right,
+	Body
 } from "native-base";
 
 import styles from "./styles";
 
 class TheParking extends Component {
+	constructor() {
+		super();
+		this.state = {
+			parkingLotInformation: null
+		};
+	}
+
+	// Call API to fetch information. When it finishes, it promises to fill the states with parking lot data.
+	getParkingLotInformation() {
+		getAllParkingLots().then((response) => {
+			console.log(response);
+			this.setState({
+				firstParkingLot: response
+			});
+		});
+	}
+
+	componentDidMount() {
+		this.getParkingLotInformation();
+	}
+
 	render() {
 		return (
 			<Container style={styles.container}>
