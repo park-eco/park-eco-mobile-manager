509722203e49f072db267a3b07f8270a715c860d
Lee Khai <leekhai18@gmail.com>
2018-12-19 09:51:13 +0700

    AsyncStorage for server

--- src/screens/login/serverInput.js
+++ src/screens/login/serverInput.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { View, Text } from "native-base";
 import Btn from 'react-native-micro-animated-button';
 import Dialog from "react-native-dialog";
-
+import { AsyncStorage } from 'react-native';
 import styles from "./styles";
 
 class ServerInput extends Component {
@@ -22,10 +22,12 @@ class ServerInput extends Component {
     }
 
     onSecondaryPress = () => {
-        this.setState({ width: 60, 
-                        clicked: false,
-                        server: "",
-                        status: "No server are running!" }, () => this.btn.reset());
+        this.setState({
+            width: 60,
+            clicked: false,
+            server: "",
+            status: "No server are running!"
+        }, () => this.btn.reset());
     }
 
     handleCancel = () => {
@@ -33,13 +35,46 @@ class ServerInput extends Component {
     }
 
     handleRunServer = () => {
-        this.setState({ width: 40, 
-                        dialogVisible: false,
-                        status: `Runing on ${this.state.server}` }, () => this.btn.success());
+        this.setState({
+            width: 40,
+            dialogVisible: false,
+            status: `Runing on ${this.state.server}`
+        }, () => {
+            this.btn.success();
+            this._storeData();
+        });
     }
 
     onChangeText = (value) => {
         this.state.server = value;
+    }
+
+    _storeData = async () => {
+        try {
+            await AsyncStorage.setItem('SERVER', this.state.server);
+        } catch (error) {
+            // Error saving data
+            console.log(error);
+        }
+    }
+
+    _retrieveData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('SERVER');
+            if (value !== null) {
+                // We have data!!
+                console.log(value);
+                this.state.server = value;
+                this.handleRunServer();
+            }
+        } catch (error) {
+            // Error retrieving data
+            console.log(error);
+        }
+    }
+
+    componentDidMount() {
+        this._retrieveData();
     }
 
 
@@ -59,13 +94,13 @@ class ServerInput extends Component {
                     successIcon="retweet"
                 />
 
-                 <Dialog.Container visible={this.state.dialogVisible}>
+                <Dialog.Container visible={this.state.dialogVisible}>
                     <Dialog.Title>Setting server</Dialog.Title>
                     <Dialog.Description>
                         Please enter your domain server!
                     </Dialog.Description>
-                    <Dialog.Input   placeholder="Your domain server"
-                                    onChangeText={this.onChangeText}/>
+                    <Dialog.Input placeholder="Your domain server"
+                        onChangeText={this.onChangeText} />
                     <Dialog.Button label="Cancel" onPress={this.handleCancel} />
                     <Dialog.Button label="Run" onPress={this.handleRunServer} />
                 </Dialog.Container>
